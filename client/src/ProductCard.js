708108202bd5e9c56642ce8e0703a2de9385d723
Dillon Tiner <dillontiner@gmail.com>
2021-05-14 10:00:23 -0400

    basic material UI card

--- client/src/ProductCard.js
+++ client/src/ProductCard.js
@@ -1,9 +1,29 @@
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+});
+
 function ProductCard(props) {
+  const classes = useStyles();
+
   return (
-    <div>
-      <img src={props.product.image_path} className="product-image" alt={props.product.name} />
-      <p> this is a product card </p>
-    </div>
+    <Card className={classes.root}>
+      <CardContent>
+        <img src={props.product.image_path} className="product-image" alt={props.product.name} />
+        <Typography variant="h5" component="h2">
+          {props.product.ticker}
+        </Typography>
+        <Typography variant="h5" component="h2">
+          $TO.DO
+        </Typography>
+      </CardContent>
+    </Card>
   );
 }
 
