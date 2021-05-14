961cce06b61e0faf311252388249fe8556510d4e
Dillon Tiner <dillontiner@gmail.com>
2021-05-14 08:17:01 -0400

    change port number for backend api

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 
 app.get('/', (req, res) => {
   products = [{name: 'Designer Brands Inc', ticker: 'DBI', image_path: 'TODO'}]
