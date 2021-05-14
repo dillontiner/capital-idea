790ff95bab151d2c07d00f1bebd71abcff09d678
Dillon Tiner <dillontiner@gmail.com>
2021-05-14 09:43:47 -0400

    basic material UI grid

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import Axios from "axios";
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+}));
 
 function App() {
   const [products, setProducts] = useState([]);
+  const classes = useStyles();
 
   useEffect(() => {
     Axios({
@@ -18,12 +27,17 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      {console.log(products)}
-      <header className="App-header">
-        {products.map(product => <img src={product.image_path} className="product-image" alt="Lowe's Companies Inc" />)}
-      </header>
-    </div>
+    <Grid container className={classes.root} spacing={2}>
+      <Grid item xs={12}>
+        <Grid container justify="center" spacing={2}>
+          {products.map((product) => (
+            <Grid item>
+              <img src={product.image_path} className="product-image" alt="Lowe's Companies Inc" />
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
 
