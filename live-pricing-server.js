179045af5f44fd32c082abe1aa99436984319d9d
Dillon Tiner <dillontiner@gmail.com>
2021-05-14 15:49:15 -0400

    separate responsibilities of main product api server and live pricing service

--- live-pricing-server.js
+++ live-pricing-server.js
@@ -2,27 +2,16 @@ const express = require('express')
 const http = require('http');
 const cors = require("cors");
 const { Server } = require("socket.io");
-
-const app = express()
 const port = 4000
+const app = express()
+app.use(cors());
+
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
     origin: "http://127.0.0.1:" + port,
   }
-});
-
-app.use(cors());
-
-app.use(express.static('public'));
-
-app.get('/products', (req, res) => {
-  products = [
-    {name: "Lowe's Companies Inc", ticker: 'LOW', image_path: 'lowes_stock_tee.jpg'},
-    {name: "Alphabet Inc Class C", ticker: 'GOOG', image_path: 'google_stock_tee.jpg'}
-  ];
-  res.json(products);
 });
 
 io.on('connection', (socket) => {
@@ -36,5 +25,5 @@ io.on('connection', (socket) => {
 });
 
 server.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Live pricing service listening at http://localhost:${port}`);
 });
