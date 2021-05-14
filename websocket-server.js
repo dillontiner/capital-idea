807ce1ac4ee83de1be567d38e83bc3953d4d65ee
Dillon Tiner <dillontiner@gmail.com>
2021-05-14 15:44:51 -0400

    fork server to websocket server on port 4000, run all three servers in dev

---
+++ websocket-server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const http = require('http');
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const app = express()
+const port = 4000
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://127.0.0.1:" + port,
+  }
+});
+
+app.use(cors());
+
+app.use(express.static('public'));
+
+app.get('/products', (req, res) => {
+  products = [
+    {name: "Lowe's Companies Inc", ticker: 'LOW', image_path: 'lowes_stock_tee.jpg'},
+    {name: "Alphabet Inc Class C", ticker: 'GOOG', image_path: 'google_stock_tee.jpg'}
+  ];
+  res.json(products);
+});
+
+io.on('connection', (socket) => {
+  var pricing = {LOW: 0, GOOG: 0};
+  setInterval(() => {
+    socket.emit('pricing', pricing);
+    Object.keys(pricing).forEach((key) => {
+      pricing[key]++;
+    });
+  }, 1000);
+});
+
+server.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
