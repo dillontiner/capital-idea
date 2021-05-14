f70cb228a46c34a4c8c1498f0c61e77a0182d644
Dillon Tiner <dillontiner@gmail.com>
2021-05-14 08:38:37 -0400

    add axios to get products from backend, just console logging

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Axios from "axios";
 
 function App() {
+  Axios({
+    method: "GET",
+    url: "http://localhost:5000/",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(res => {
+    console.log(res.data);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
