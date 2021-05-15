bdee579423505734e8b0ad11b93a4fe52e54c748
Dillon Tiner <dillontiner@gmail.com>
2021-05-15 10:44:34 -0400

    add checkout route that succeeds with local POST in terminal

--- product-api/server.js
+++ product-api/server.js
@@ -15,6 +15,31 @@ app.get('/products', (req, res) => {
   res.json(products)
 })
 
+const stripe = require('stripe')('sk_test_51IbGTDIL6q5pLIOCsCpolhmChNK53k1UWQ2sX8f0Uq0RwxsKevLgr09lbNCYcBlba2bN4sI8fG3HDxuoyvTeqnff00E5dAx3zs')
+
+app.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: 'T-shirt',
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: 'https://example.com/success',
+    cancel_url: 'https://example.com/cancel',
+  });
+
+  res.json({ id: session.id });
+});
+
 app.listen(port, () => {
   console.log(`Product service listening at http://localhost:${port}`)
 })
